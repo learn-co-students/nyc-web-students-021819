2399cbce5855f25d79f41a171b79ead489fc93af
Alex Griffith <alexalexgriffith@gmail.com>
2019-03-01 10:22:50 -0500

    Pre lecture 09 setup MESSAGE TO FUTURE ALEX-USE THIS ONE

--- 09-dynamic-orms/config/environment.rb
+++ 09-dynamic-orms/config/environment.rb
@@ -1,11 +1,25 @@
 require 'bundler'
 Bundler.require
 
+# Connection to DB
+# ActiveRecord::Base.establish_connection(
+#   {
+#     adapter: 'sqlite3',
+#     database: 'db/twitter.db'
+#   }
+# )
+
+# Logger
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+
 DB = {
   conn: SQLite3::Database.new('db/twitter.db')
 }
 
 DB[:conn].results_as_hash = true
+
+
 
 sql = <<SQL
   CREATE TABLE
@@ -25,12 +39,13 @@ sql = <<SQL
   (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT
+    email TEXT
   );
 SQL
 
 DB[:conn].execute(sql)
 
-require_relative '../lib/dynamic_record.rb'
+# require_relative '../lib/passive_record.rb'
 require_relative '../lib/user.rb'
 require_relative '../lib/tweet.rb'
 require_relative '../lib/tweets_app.rb'
