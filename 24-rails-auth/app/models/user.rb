1285e51df82e9341f3f5d9e22bdd09716a938d99
Wangtron <evansjwang@gmail.com>
2019-03-25 14:18:22 -0400

    AUTH AUTH AUTH

---
+++ 24-rails-auth/app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  # validates :password_digest
+  # def password
+  #   @password
+  # end
+  #
+  # # pt_pw = plainttext_passwrod
+  # def password=(pt_pw)
+  #   self.password_digest = BCrypt::Password.create(pt_pw)
+  # end
+  #
+  # #
+  # def authenticate(pt_pw) # 'goat'
+  #   # compare an already existing user's plaintext_pw against the salted and hashed version we have in the db
+  #    if BCrypt::Password.new(self.password_digest) == pt_pw
+  #
+  # end
+
+
+end
